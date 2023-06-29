aae663507fa0af7cd4aa4b733fc9ba7a1e2a72b5
Felipe Palma <felipepalmacastro@gmail.com>
2023-06-28 21:56:05 -0400

    Operativo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { FileUploadService } from "./service/file-upload.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = '0080_CL_WebThreat_Front_NDS';
+  selectedFileName: string = '';
+  selectedFileURL: string | ArrayBuffer | null = null;
+
+  constructor(fileUploadService: FileUploadService) {
+  }
+
+  handleFileSelect(event: any) {
+    const selectedFile = event.target.files[0];
+    if (selectedFile) {
+      this.selectedFileName = selectedFile.name;
+      this.loadSelectedFile(selectedFile);
+      console.log(this.selectedFileName);
+    }
+  }
+
+  loadSelectedFile(file: File) {
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      this.selectedFileURL = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
 }
+
+
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +20,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
